90336d26aa088249c00f7e079604eaca63bc12a1
ariel- <ariel-@users.noreply.github.com>
2017-03-27 02:59:26 -0300

    Core/Global: reserve storage for script names vector
    (cherry picked from commit c8bf1ab5f7e2e44e37c5affbf69c64917dc45c9e)

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -9492,6 +9492,8 @@ void ObjectMgr::LoadScriptNames()
         return;
     }
 
+    _scriptNamesStore.reserve(result->GetRowCount() + 1);
+
     do
     {
         _scriptNamesStore.push_back((*result)[0].GetString());
