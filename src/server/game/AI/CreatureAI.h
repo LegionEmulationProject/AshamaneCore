8c90795b3af0252e5b3398898764bcaf8059d58d
ariel- <ariel-@users.noreply.github.com>
2017-03-28 01:52:49 -0300

    Core/AI: AreaBoundary refactor
    - Added an auxiliary function IsInBounds to base CreatureAI
    - Changed container to vector. Set had no sense because we're storing new pointers, they have different addresses even if the boundary is the same
    
    (cherry picked from commit 2f99fa09c9bacbad376d7a296c3311f94ec8a552)

--- src/server/game/AI/CreatureAI.h
+++ src/server/game/AI/CreatureAI.h
@@ -34,7 +34,7 @@ class WorldObject;
 struct Position;
 struct SpellDestination;
 
-typedef std::set<AreaBoundary const*> CreatureBoundary;
+typedef std::vector<AreaBoundary const*> CreatureBoundary;
 
 #define TIME_INTERVAL_LOOK   5000
 #define VISIBILITY_RANGE    10000
@@ -85,6 +85,8 @@ class TC_GAME_API CreatureAI : public UnitAI
         Creature* DoSummonFlyer(uint32 entry, WorldObject* obj, float flightZ, float radius = 5.0f, uint32 despawnTime = 30000, TempSummonType summonType = TEMPSUMMON_CORPSE_TIMED_DESPAWN);
 
         bool CheckBoundary(Position const* who = nullptr) const;
+        static bool IsInBounds(CreatureBoundary const* boundary, Position const* who);
+
         void SetBoundary(CreatureBoundary const* boundary);
     public:
         enum EvadeReason
