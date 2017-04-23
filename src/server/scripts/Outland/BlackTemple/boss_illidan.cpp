1962a77c6ccbacd0469244c8996bcaccc381426d
ariel- <ariel-@users.noreply.github.com>
2017-04-23 20:38:14 -0300

    Core/Spell: define channels without movement interrupt flags as allowed move
    - Remove script hacks no longer needed
    
    (cherry picked from commit 3a0cb90ea994e82dd8c70888fb847082f738d5dc)

--- src/server/scripts/Outland/BlackTemple/boss_illidan.cpp
+++ src/server/scripts/Outland/BlackTemple/boss_illidan.cpp
@@ -2300,7 +2300,6 @@ class spell_illidan_find_target : public SpellScriptLoader
                 if (Creature* caster = GetCaster()->ToCreature())
                 {
                     caster->CastSpell(target, SPELL_PARALYZE, true);
-                    caster->ClearUnitState(UNIT_STATE_CASTING);
                     caster->AI()->SetGUID(target->GetGUID(), 0);
                 }
             }
