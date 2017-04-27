f09526bca68ee117504f37521066d6ad54f17655
ccrs <ccrs@users.noreply.github.com>
2017-04-27 14:00:57 +0200

    Core/CreatureAI: b6b0353bff followup
    (cherry picked from commit 5fc366d03bf4e5250443e0573a4c9fa4611968a4)

--- src/server/game/AI/CreatureAI.cpp
+++ src/server/game/AI/CreatureAI.cpp
@@ -218,7 +218,7 @@ void CreatureAI::SetGazeOn(Unit* target)
 {
     if (me->IsValidAttackTarget(target))
     {
-        if (!me->IsFocusing(nullptr, true))
+        if (!me->IsFocusing(nullptr, true) && target != me->GetVictim())
             AttackStart(target);
         me->SetReactState(REACT_PASSIVE);
     }
@@ -238,7 +238,7 @@ bool CreatureAI::UpdateVictimWithGaze()
     }
 
     if (Unit* victim = me->SelectVictim())
-        if (!me->IsFocusing(nullptr, true))
+        if (!me->IsFocusing(nullptr, true) && victim != me->GetVictim())
             AttackStart(victim);
 
     return me->GetVictim() != nullptr;
