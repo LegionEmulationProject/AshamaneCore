77f1abd005d3ecb89c7b889cfcaadf908905bb8c
Shauren <shauren.trinity@gmail.com>
2020-02-07 20:11:27 +0100

    Core/Misc: Moved CanSpeak() method to WorldSession class

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -21920,11 +21920,6 @@ void Player::UpdateSpeakTime()
     m_speakTime = current + sWorld->getIntConfig(CONFIG_CHATFLOOD_MESSAGE_DELAY);
 }
 
-bool Player::CanSpeak() const
-{
-    return  GetSession()->m_muteTime <= time (nullptr);
-}
-
 /*********************************************************/
 /***              LOW LEVEL FUNCTIONS:Notifiers        ***/
 /*********************************************************/
