5700baba81a6d20c961a8a0702e724d0ec2a2dc4
Shauren <shauren.trinity@gmail.com>
2020-03-26 20:09:40 +0100

    Core/DataStore: Load ItemNameDescription

--- src/server/game/DataStores/DB2Structure.h
+++ src/server/game/DataStores/DB2Structure.h
@@ -1707,6 +1707,13 @@ struct ItemModifiedAppearanceEntry
     int8 TransmogSourceTypeEnum;
 };
 
+struct ItemNameDescriptionEntry
+{
+    uint32 ID;
+    LocalizedString* Description;
+    int32 Color;
+};
+
 struct ItemPriceBaseEntry
 {
     uint32 ID;
