16ef2cfe722fe379f718e81bc7bf531f3ac2a079
TheGhostGroup <theghostgroup@gmail.com>
2023-03-31 18:15:52 +1300

    DB/Character: Add character templates.
    *Players can now use templates to create characters.

---
+++ sql/ashamane/world/2023_03_31_01_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `character_template` WHERE `ID` IN (37, 38);
+INSERT INTO `character_template` (`ID`, `Name`, `Description`, `Level`) VALUES
+(37, '100 level character', 'Create Level 100 character', 100),
+(38, '110 level character', 'Create Level 100 character', 110);
