a094b75b8bc02956157e8e593432443dc7530689
TheGhostGroup <theghostgroup@gmail.com>
2023-04-03 20:03:14 +1200

    DB/Creature: Emissary Auldbridge
    *Update ScriptName.

--- sql/ashamane/world/2023_04_01_02_world.sql
+++ sql/ashamane/world/2023_04_01_02_world.sql
@@ -1,2 +1,2 @@
 -- Add Script To Auldbridge
-UPDATE `creature_template` SET `ScriptName`='npc_emmisary_auldbridge_111109' WHERE `entry`=111109;
+UPDATE `creature_template` SET `ScriptName`='npc_emissary_auldbridge_111109' WHERE `entry`=111109;
