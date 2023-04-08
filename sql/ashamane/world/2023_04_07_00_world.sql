4c6f41d76abed6da4bc0e3f04086e9c523445351
TheGhostGroup <theghostgroup@gmail.com>
2023-04-08 14:53:10 +1200

    fix typo

--- sql/ashamane/world/2023_04_07_00_world.sql
+++ sql/ashamane/world/2023_04_07_00_world.sql
@@ -4,4 +4,4 @@ DELETE FROM `creature` WHERE `map` = 1479;
 DELETE FROM `creature_addon` WHERE `guid` NOT IN (SELECT `guid` FROM `creature`);
 
 DELETE FROM `gameobject` WHERE `map` = 1479;
-DELETE FROM `gameoejct_addon` WHERE `guid` NOT IN (SELECT `guid` FROM `gameobject`);
+DELETE FROM `gameobject_addon` WHERE `guid` NOT IN (SELECT `guid` FROM `gameobject`);
