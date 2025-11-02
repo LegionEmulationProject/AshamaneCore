ef5e90c4c0636514ae28eb06c1b3557e6d1f6721
TheGhost <theghostgroup@gmail.com>
2025-11-02 15:58:19 +1300

    Game/SAI: Implemented SMART_ACTION_CREATE_CONVERSATION

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -568,6 +568,8 @@ enum SMART_ACTION
     SMART_ACTION_PLAY_ANIMKIT                       = 128,    // id, type (0 = oneShot, 1 = aiAnim, 2 = meleeAnim, 3 = movementAnim, 4 = spellVisualKit)
     SMART_ACTION_SCENE_PLAY                         = 129,    // sceneId
     SMART_ACTION_SCENE_CANCEL                       = 130,    // sceneId
+    SMART_ACTION_CREATE_CONVERSATION                = 143,    // conversation_template.id
+    
     // 131 - 135 : 3.3.5 reserved
     SMART_ACTION_PLAY_CINEMATIC                     = 135,    // reserved for future uses
 
@@ -1206,6 +1208,11 @@ struct SmartAction
             uint32 spellId;
             uint32 triggered;
         } castOffSet;
+
+        struct
+        {
+            uint32 id;
+        } conversation;        
         //! Note for any new future actions
         //! All parameters must have type uint32
 
