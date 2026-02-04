allankrdec/TrinityCore-10-2024

Core/Quests: changed quest_template_addon.NextQuestID to unsigned
- No reason to keep this anymore after 4349adf0f25210e5b26201523a09bc26b4a83394, this is a broken functionality.

src/server/game/Globals/ObjectMgr.cpp
@@ -4588,14 +4588,11 @@ void ObjectMgr::LoadQuests()
 
         if (qinfo->_nextQuestId)
         {
-            auto qNextItr = _questTemplates.find(std::abs(qinfo->GetNextQuestId()));
+            auto qNextItr = _questTemplates.find(qinfo->GetNextQuestId());
             if (qNextItr == _questTemplates.end())
-                TC_LOG_ERROR("sql.sql", "Quest %u has NextQuestId %i, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
+                TC_LOG_ERROR("sql.sql", "Quest %u has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
             else
-            {
-                int32 signedQuestId = qinfo->_nextQuestId < 0 ? -int32(qinfo->GetQuestId()) : int32(qinfo->GetQuestId());
-                qNextItr->second->PrevQuests.push_back(signedQuestId);
-            }
+                qNextItr->second->PrevQuests.push_back(static_cast<int32>(qinfo->GetQuestId()));
         }
 
         if (qinfo->_exclusiveGroup)

LegionEmulationProject/AshamaneCore

(cherry picked from commit dca276716fecb819fec668f59954113a77f9e393)

src/server/game/Globals/ObjectMgr.cpp
@@ -4757,14 +4757,11 @@ void ObjectMgr::LoadQuests()
 
         if (qinfo->_nextQuestID)
         {
-            QuestMap::iterator qNextItr = _questTemplates.find(abs(qinfo->GetNextQuestId()));
+            auto qNextItr = _questTemplates.find(qinfo->GetNextQuestId());
             if (qNextItr == _questTemplates.end())
-                TC_LOG_ERROR("sql.sql", "Quest %d has NextQuestId %i, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
+                TC_LOG_ERROR("sql.sql", "Quest %d has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
             else
-            {
-                int32 signedQuestId = qinfo->_nextQuestID < 0 ? -int32(qinfo->GetQuestId()) : int32(qinfo->GetQuestId());
-                qNextItr->second->PrevQuests.push_back(signedQuestId);
-            }
+                qNextItr->second->PrevQuests.push_back(static_cast<int32>(qinfo->GetQuestId()));
         }
 
         if (qinfo->_exclusiveGroup)