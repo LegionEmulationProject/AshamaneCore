029c9966126d4b8556d6642c7fecdb2308d4fb37
TheGhost <theghostgroup@gmail.com>
2026-02-15 12:26:59 +1300

    Core/DBLayer: Fix build using TRINITY_DEBUG

--- src/server/database/Database/DatabaseWorkerPool.cpp
+++ src/server/database/Database/DatabaseWorkerPool.cpp
@@ -249,7 +249,7 @@ void DatabaseWorkerPool<T>::CommitTransaction(SQLTransaction<T> transaction)
     {
     case 0:
         TC_LOG_DEBUG("sql.driver", "Transaction contains 0 queries. Not executing.");
-        return;
+        break;
     case 1:
         TC_LOG_DEBUG("sql.driver", "Warning: Transaction only holds 1 query, consider removing Transaction context in code.");
         break;
