24defe7e90c91babcbca436055d25a19f9ef7421
TheGhost <theghostgroup@gmail.com>
2026-04-06 19:27:22 +1200

    DB/Transports: Add battle for brokenshore transports.

---
+++ sql/ashamane/world/2026_04_06_01_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `transports` WHERE `guid` IN (29, 30);
+INSERT INTO `transports` (`guid`, `entry`, `name`, `phaseUseFlags`, `phaseid`, `phasegroup`, `ScriptName`) VALUES 
+(29, 251513, 'Battle for Broken Shore Alliance Ship', 0, 0, 0, ''),
+(30, 254124, 'Battle for Broken Shore Horde Ship', 0, 0, 0, '');
