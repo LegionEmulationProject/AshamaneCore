fa315be1ad1ba1a9ca465313a6ac9abec59dfcad
TheGhost <theghostgroup@gmail.com>
2026-04-12 14:24:06 +1200

    DB/Terrainswap: Add missing terrain swap defaults for blastedlands draenor phase.

---
+++ sql/ashamane/world/2026_04_12_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `terrain_swap_defaults` WHERE `MapId`= 0 AND `TerrainSwapMap`= 1190;
+INSERT INTO `terrain_swap_defaults` (`MapId`,`TerrainSwapMap`,`Comment`) VALUES
+(0, 1190, 'Blasted Land Terrian');
