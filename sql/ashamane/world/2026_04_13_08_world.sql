d6336080462320d94a454f46e0f7eb2bf36f59dc
TheGhost <theghostgroup@gmail.com>
2026-04-13 20:26:39 +1200

    DB/Equip: Add missing equipment template to Khadgar.

---
+++ sql/ashamane/world/2026_04_13_08_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature_equip_template` WHERE `CreatureID`=78558;
+INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `AppearanceModID1`, `ItemVisual1`, `ItemID2`, `AppearanceModID2`, `ItemVisual2`, `ItemID3`, `AppearanceModID3`, `ItemVisual3`, `VerifiedBuild`) VALUES
+(78558, 1, 28067, 0, 0, 0, 0, 0, 0, 0, 0, 0); -- 78558
