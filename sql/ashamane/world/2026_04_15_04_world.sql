a7396c4f67390b72e7d3c25b31eb9f0098415803
TheGhost <theghostgroup@gmail.com>
2026-04-15 18:20:07 +1200

    DB/Creature: Update Qianna Moonshadow dark portal spawn.
    * Add correct phase to spawn
    * Update guid to correct range for expansion.

---
+++ sql/ashamane/world/2026_04_15_04_world.sql
@@ -0,0 +1,2 @@
+SET @CGUID := 500033;
+UPDATE `creature` SET `PhaseId`=3568, `guid`=@CGUID+0, `position_x`=4010.16796875, `position_y`=-2349.280029296875, `position_z`=73.3446197509765625, `orientation`=1.040212273597717285 WHERE `guid`=1385318;
