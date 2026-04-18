cd03164430db0f5e7a0e91925927e1e000eab838
TheGhost <theghostgroup@gmail.com>
2026-04-18 19:17:57 +1200

    DB/Gameobject: Gul'dan Door Light B
    * Added correct PhaseGroup to spawns.
    * Updated guid to correct range for expansion.

---
+++ sql/ashamane/world/2026_04_18_09_world.sql
@@ -0,0 +1,6 @@
+SET @OGUID := 500014;
+UPDATE `gameobject` SET `PhaseGroup`=856, `guid`=@OGUID+0 WHERE `guid`=1249914;
+UPDATE `gameobject` SET `PhaseGroup`=856, `guid`=@OGUID+1 WHERE `guid`=1249916;
+UPDATE `gameobject` SET `PhaseGroup`=856, `guid`=@OGUID+2 WHERE `guid`=1249917;
+UPDATE `gameobject` SET `PhaseGroup`=865, `guid`=@OGUID+3 WHERE `guid`=1249921;
+
