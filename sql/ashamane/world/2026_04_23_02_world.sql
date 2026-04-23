7a867b36b28d9a583d50918d25749d5ee31dfa62
TheGhost <theghostgroup@gmail.com>
2026-04-23 18:09:08 +1200

    fix

--- sql/ashamane/world/2026_04_23_02_world.sql
+++ sql/ashamane/world/2026_04_23_02_world.sql
@@ -1,5 +1,5 @@
 -- Destroyed Iron Horde War Machine Type 01
-SET @OGUID := 500018;
+SET @OGUID := 500025;
 UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+0 WHERE `guid`=1250182;
 UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+1 WHERE `guid`=1250122;
 UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+2 WHERE `guid`=1250099;
