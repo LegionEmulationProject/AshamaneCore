731607d1d492db4f2b29c3d288696bd247d0bc6f
TheGhost <theghostgroup@gmail.com>
2026-04-23 14:22:46 +1200

    DB/Gameobject: Destroyed Iron Horde War Machine Type 01
    * Add correct phase to spawn.
    * Update guid to correct range for expansion.

---
+++ sql/ashamane/world/2026_04_23_02_world.sql
@@ -0,0 +1,8 @@
+-- Destroyed Iron Horde War Machine Type 01
+SET @OGUID := 500018;
+UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+0 WHERE `guid`=1250182;
+UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+1 WHERE `guid`=1250122;
+UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+2 WHERE `guid`=1250099;
+UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+3 WHERE `guid`=1249961;
+UPDATE `gameobject` SET `PhaseId`=3568, `guid`=@OGUID+4 WHERE `guid`=1249905;
+
