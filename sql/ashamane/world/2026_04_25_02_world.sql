e7ae68d4494be3758176a19fbf904cd2e32b51c5
TheGhost <theghostgroup@gmail.com>
2026-04-25 18:11:36 +1200

    DB/Phase: Add missing phase area data.

---
+++ sql/ashamane/world/2026_04_25_02_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `phase_area` WHERE `PhaseId`=7138;
+INSERT INTO `phase_area` (`AreaId`, `PhaseId`, `Comment`) VALUES
+(7502, 7138, 'See Emissary Auldbridge, welcoming in Broken Isles Dalaran');
