11db8fc07c84135f40c15d9dcf50147b7c689346
TheGhost <theghostgroup@gmail.com>
2026-05-01 16:03:56 +1200

    DB/Creature: Meryl Felstorm creature text

---
+++ sql/ashamane/world/2026_05_01_00_world.sql
@@ -0,0 +1,22 @@
+DELETE FROM `creature_text` WHERE `CreatureId`=102700;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `comment`) VALUES
+(102700, 0, 0, 'It seems my fears were well-founded! The dreadlord is already inside. We must hurry!', 12, 0, 100, 603, 0, 69708, 120632, 'Meryl·Felsong to Player'),
+(102700, 1, 0, 'The hall hasn''t seen any use since the disbanding of the Council of Tirisfal.', 12, 0, 100, 0, 0, 61731, 105438, 'Meryl·Felsong to Player'),
+(102700, 2, 0, 'Look to the stacks for answers. I will check my memory vault.', 12, 0, 100, 1, 0, 64246, 112312, 'Meryl·Felsong to Player'),
+(102700, 3, 0, 'So Arrexis was ambushed by this eredar, Balaadur?  Then he is probably in possession of the staff...', 12, 0, 100, 1, 0, 64241, 112317, 'Meryl·Felsong to Player'),
+(102700, 4, 0, 'With the invasion I have no doubt he is either here or watching. I propose something... audacious.', 12, 0, 100, 1, 0, 64243, 112326, 'Meryl·Felsong to Player'),
+(102700, 5, 0, 'Let us recreate Arrexis''s ritual! Conducting the ritual at an invasion point will attract Balaadur''s attention. He will likely try an ambush, but we will be ready.', 12, 0, 100, 1, 0, 64244, 112327, 'Meryl·Felsong to Player'),
+(102700, 6, 0, 'In the center of Dalaran is a portal that can you to Karazhan. Start your search in the mountains west of the tower. I seem to recall a mention of something up there.', 12, 0, 100, 1, 0, 64247, 112462, 'Meryl·Felsong to Player'),
+(102700, 7, 0, 'Uh, I''m pretty sure she''s a goblin now.', 12, 0, 100, 11, 0, 64239, 112459, 'Meryl·Felsong to Player'),
+(102700, 8, 0, 'I will arrange transportation at Krasus'' Landing. Talk to the flight master when you are ready.', 12, 0, 100, 1, 0, 64248, 112812, 'Meryl·Felsong to Player'),
+(102700, 9, 0, 'Alright Akazamzarak, you have the locations. Do you think you can manage the portals?', 12, 0, 100, 0, 0, 61738, 105822, 'Meryl·Felsong to Player'),
+(102700, 10, 0, 'Mages of Azeroth! I have summoned you here because we face a threat to the future of the world itself!', 12, 0, 100, 0, 0, 61740, 105824, 'Meryl·Felsong to Player'),
+(102700, 11, 0, 'The dreadlord Kathra\'natir has escaped into the Twisting Nether, carrying with him the secrets of the Council of Tirisfal.', 12, 0, 100, 0, 0, 61742, 105825, 'Meryl·Felsong to Player'),
+(102700, 12, 0, 'We must hunt him down in the Twisting Nether. Only then will we know his knowledge cannot be used against us.', 12, 0, 100, 0, 0, 61743, 105826, 'Meryl·Felsong to Player'),
+(102700, 13, 0, 'To this end, I hereby reform the Tirisgarde. Will you join us and take up arms against the Legion?', 12, 0, 100, 0, 0, 61744, 105827, 'Meryl·Felsong to Player'),
+(102700, 14, 0, 'It is done, then.', 12, 0, 100, 1, 0, 61745, 105828, 'Meryl·Felsong to Player'),
+(102700, 15, 0, 'Very well. Here, in presence of many of Azeroth\'s greatest mages, it is my honor to dub you $n, Conjuror of the Tirisgarde.', 12, 0, 100, 1, 0, 61746, 105829, 'Meryl·Felsong to Player'),
+(102700, 16, 0, 'This ancient title symbolizes the awesome responsibility borne by the Tirisgarde. May you carry it with honor.', 12, 0, 100, 1, 0, 61747, 105830, 'Meryl·Felsong to Player'),
+(102700, 17, 0, 'It is time to meet with our new goblin ally in the War Room. You can begin planning your next steps there.', 12, 0, 100, 0, 0, 61748, 105831, 'Meryl·Felsong to Player'),
+(102700, 18, 0, 'I have some personal business to attend to...', 12, 0, 100, 0, 0, 61749, 105993, 'Meryl·Felsong to Player');
+
