4bab0afaf2abf8b1bf99d7db6950ea90983f6f5b
TheGhost <theghostgroup@gmail.com>
2026-06-23 20:44:00 +1200

    DB/Cleanup: Fix a few more DB Errors.

---
+++ sql/ashamane/world/2026_06_23_00_world.sql
@@ -0,0 +1,9 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_bank_67497', 'spell_bank_67499', 'spell_launch_96185', 'spell_mulgore_funeral_offering', 'spell_68281',
+'spell_bank_67496', 'spell_bank_67498', 'spell_gen_moss_covered_feet', 'spell_kezan_despawn_sharks', 'spell_rescue_drowning_watchman_68735', 'spell_round_up_horse_68903');
+
+UPDATE `creature_template` SET `ScriptName`='' WHERE `ScriptName` IN ('npc_girocoptere', 'npc_ashley_36269', 'npc_blood_ritual_orb', 'npc_blackrock_follower', 'npc_archmage_khadgar_bridge',
+'npc_archmage_khadgar', 'npc_chipie_quest_giver_end_event', 'npc_ankova_the_fallen', 'npc_tanaan_yrel_summon', 'npc_galaw', 'npc_eagle_spirit', 'npc_bleeding_hollow_sauvage',
+'npc_ogron_warcrusher', 'npc_james_36268', 'npc_tanaan_mandragora', 'npc_maladaar_liadrin_tanaan_cave', 'npc_blackrock_grunt', 'npc_iron_gronnling', 'npc_cynthia_36267', 
+'npc_thaelin_tanaan_questgiver', 'npc_lianne_gobelin', 'npc_generic_tanaan_guardian', 'npc_thrall_maladaar_blackrock', 'npc_gls_gob', 'npc_keli_dan_the_breaker', 'npc_meteor_gob');
+
+
