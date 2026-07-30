834d487af1685646609169af7afe20a747f062ab
TheGhost <theghostgroup@gmail.com>
2026-07-30 14:40:02 +1200

    DB/Misc: Clean up some logs

---
+++ sql/ashamane/world/2026_07_31_01_world.sql
@@ -0,0 +1,4 @@
+UPDATE `gameobject_template` SET `scriptname`='' WHERE `entry`=196432;
+UPDATE `creature` SET `ScriptName`='' WHERE `guid` IN (192770, 192772);
+UPDATE `creature_template` SET `scriptname`='' WHERE `entry` IN (80244, 2000008, 82057, 2000001, 78994, 85145, 85146, 80803, 78556, 79917, 86039, 86690, 86780, 80574,
+79583, 79585, 78568, 42763, 79097, 78561, 42015);
