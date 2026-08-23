aaee7b7947edfa2a399b4203e0fea2dec6dea2eb
TheGhost <theghostgroup@gmail.com>
2026-08-23 15:15:51 +1200

    Core/Loot: implement Loot Item Storage (#19018)
    * Created Item Loot Storage, no more synchronous DB selects
    * Fixed buyback case, where stored loot was not removed from db
    * Added Primary key, and changed field types to be unsigned for table item_loot_money
    
    (cherry picked from commit 9dc3de10f0044c35a95e9b72e7b874f4b5b8e867)

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -2167,8 +2167,8 @@ CREATE TABLE `item_loot_items` (
 DROP TABLE IF EXISTS `item_loot_money`;
 
 CREATE TABLE `item_loot_money` (
-  `container_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'guid of container (item_instance.guid)',
-  `money` int(10) NOT NULL DEFAULT '0' COMMENT 'money loot (in copper)',
+  `container_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'guid of container (item_instance.guid)',
+  `money` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'money loot (in copper)',
   PRIMARY KEY (`container_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
