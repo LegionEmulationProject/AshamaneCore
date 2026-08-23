allankrdec/TrinityCore-10-2024

Core/Misc: pch fixes pt 2

src/server/game/Loot/LootItemStorage.h
@@ -19,6 +19,7 @@
 #define __LOOTITEMSTORAGE_H
 
 #include "Define.h"
+#include "Transaction.h"
 
 #include <unordered_map>
 

LegionEmulationProject/AshamaneCore

(cherry picked from commit e32a4f26aaa51fba2647aeb8ef751dd398bc09d8)

src/server/game/Loot/LootItemStorage.h
@@ -18,6 +18,7 @@
 #ifndef __LOOTITEMSTORAGE_H
 #define __LOOTITEMSTORAGE_H
 
+#include "DatabaseEnvFwd.h"
 #include "Define.h"
 #include "DBCEnums.h"
 #include "ItemEnchantmentMgr.h"